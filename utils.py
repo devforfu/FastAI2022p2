90c1b72ed93e0a96fc7a7b46c2473ac9796c3cb2
devforfu <developer.z@outlook.com>
2023-01-08 21:28:11 +0100

    Cleaning and linter

--- utils.py
+++ utils.py
@@ -5,15 +5,16 @@ from urllib.request import urlretrieve
 
 MNIST_URL = "https://github.com/mnielsen/neural-networks-and-deep-learning/blob/master/data/mnist.pkl.gz?raw=true"
 
-DATA_DIR = Path(__file__).parent/"data"
+DATA_DIR = Path(__file__).parent / "data"
+
 
 def get_mnist(log=True, path_data=DATA_DIR):
     path_data.mkdir(exist_ok=True)
-    path_gz = path_data/"mnist.pkl.gz"
-    if not path_gz.exists(): 
+    path_gz = path_data / "mnist.pkl.gz"
+    if not path_gz.exists():
         urlretrieve(MNIST_URL, path_gz)
-    with gzip.open(path_gz, 'rb') as f: 
-        ((x_train, y_train), (x_valid, y_valid), _) = pickle.load(f, encoding='latin-1')
+    with gzip.open(path_gz, "rb") as f:
+        ((x_train, y_train), (x_valid, y_valid), _) = pickle.load(f, encoding="latin-1")
     data = x_train, y_train, x_valid, y_valid
     if log:
         print(f"train: x.shape={x_train.shape}, y.shape={y_train.shape}")
